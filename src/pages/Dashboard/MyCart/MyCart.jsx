618b1083fb07621b9f467e5653e0e0e802daa397
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-28 12:33:01 +0600

    77-9: Confirm before DELETE and  Delete cart item and sync data using refetch

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,31 +1,53 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useContext } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { CartContext } from '../../../contexts/CartProvider';
 import Table from './Table';
+import { Link } from 'react-router-dom';
 
 const MyCart = () => {
-  const { cart, refetech } = useContext(CartContext);
-  console.log(cart);
+  const { cart } = useContext(CartContext);
+  // console.log(cart);
 
-  const totalPrice = cart?.reduce(
-    (accumulator, item) => accumulator + item.price,
-    0
-  );
+  const totalPrice =
+    cart.length > 0 &&
+    cart?.reduce((accumulator, item) => accumulator + item.price, 0);
 
   console.log(totalPrice);
   return (
-    <div className="w-full px-10">
-      <Helmet>
-        <title>Bistro Boss | My Cart</title>
-      </Helmet>
-      <div className="flex items-center justify-between pb-10 font-semibold">
-        <h3 className="text-3xl">Total Items: {cart.length}</h3>
-        <h3 className="text-3xl">Total Price: ${totalPrice.toFixed(2)}</h3>
-        <button className="btn-warning btn-sm btn  capitalize">Pay</button>
-      </div>
+    <>
+      {cart && cart.length > 0 ? (
+        <>
+          <div className="w-full px-10">
+            <Helmet>
+              <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            <div className="flex items-center justify-between pb-10 font-semibold">
+              <h3 className="text-3xl">Total Items: {cart.length}</h3>
+              <h3 className="text-3xl">
+                Total Price: ${totalPrice.toFixed(2)}
+              </h3>
+              <button className="btn-warning btn-sm btn  capitalize">
+                Pay
+              </button>
+            </div>
 
-      <Table cart={cart} />
-    </div>
+            <Table cart={cart} />
+          </div>
+        </>
+      ) : (
+        <div className="flex items-center justify-center">
+          <h3 className="flex flex-col items-center gap-4 text-3xl font-bold">
+            <p>You haven't added any items to cart.</p>
+            <Link to="/order/:category">
+              <button className="btn-warn btn capitalize">
+                Add Item to Cart
+              </button>
+            </Link>
+          </h3>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/pages/Dashboard/MyCart/TableRow.jsx
+++ src/pages/Dashboard/MyCart/TableRow.jsx
@@ -1,7 +1,43 @@
+import { useContext } from 'react';
 import { AiOutlineDelete } from 'react-icons/ai';
+import Swal from 'sweetalert2';
+import { CartContext } from '../../../contexts/CartProvider';
 
 const TableRow = ({ item, index }) => {
-  const { name, image, price } = item;
+  const { refetch } = useContext(CartContext);
+  const { name, image, price, _id } = item;
+
+  const handleDeleteItem = (id) => {
+    // console.log(id);
+
+    //* Confirm before DELETE
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/cart/${id}`, {
+          method: 'DELETE',
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.success) {
+              refetch();
+
+              Swal.fire('Deleted!', `${data.message}`, 'success');
+            }
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      }
+    });
+  };
 
   return (
     <tr>
@@ -23,7 +59,10 @@ const TableRow = ({ item, index }) => {
 
       <td>${price}</td>
       <td>
-        <button className=" btn border-0 bg-red-500">
+        <button
+          onClick={() => handleDeleteItem(_id)}
+          className=" btn border-0 bg-red-500"
+        >
           <AiOutlineDelete size={28} />
         </button>
       </td>
