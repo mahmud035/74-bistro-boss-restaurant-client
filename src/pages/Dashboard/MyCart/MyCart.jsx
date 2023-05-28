ec173efada61ad57b3e23dfc9edfe45ba870071b
mahmud035 <mahamudulhasan4148@gmail.com>
2023-05-28 11:14:00 +0600

    MyCart: calculat totalPrice using reduce method

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,12 +1,26 @@
+import { useContext } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { CartContext } from '../../../contexts/CartProvider';
 
 const MyCart = () => {
+  const { cart, refetech } = useContext(CartContext);
+  console.log(cart);
+
+  const totalPrice = cart.reduce(
+    (accumulator, item) => accumulator + item.price,
+    0
+  );
+
+  console.log(totalPrice);
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | My Cart</title>
       </Helmet>
-      my cart
+      <div>
+        <h3 className="text-3xl">Total Items: {cart.length}</h3>
+        <h3 className="text-3xl">Total Price: ${totalPrice.toFixed(2)}</h3>
+      </div>
     </div>
   );
 };
